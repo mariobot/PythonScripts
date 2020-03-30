35fdcaf67065a762e55e08f69190da9788855c93
= <mariobot113@hotmail.com>
2020-03-29 20:26:53 -0500

    add format.py with format functionality

---
+++ 7_Input_Output/format.py
@@ -0,0 +1,12 @@
+# You can format text with f or F keyword previous of "" characters
+
+year = 2020
+month = 3
+status = "EN CUARENTENA"
+
+message = f"Es el año {year} con el mes numero {month} el estado actual es {status}"
+message2 = f"El cuadrado de 2 es {2*2}"
+
+print(message)
+print(message2)
+
