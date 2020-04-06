79f43f38fb234e0ddaf844dbf5ceecabd7020be2
mariobot113@hotmail.com <mariobot113@hotmail.com>
2020-04-05 19:19:29 -0500

    Class descriptions and funtionality

--- 9_Classes/Class.py
+++ 9_Classes/Class.py
@@ -1,3 +1,4 @@
+# reference documentation https://docs.python.org/3/tutorial/classes.html
 # To define a class use class !obius
 
 class website:
@@ -27,6 +28,63 @@ class page:
 p = page("Fernando","Entrenador")
 print(p.show_parameter1())
     
+# class and instance variables
+
+class vehicle:
+
+    brand = "Mazda"
+
+    def __init__(self, name):
+        self.name = name
+
+cls1 = vehicle("Miata")
+print(cls1.brand)
+print(cls1.name)
+cls2 = vehicle("121")
+print(cls2.brand)
+print(cls2.name)
+
+# its posible to define a methods in a class that wass writen outside of the class
+
+def sum(x,y):
+    return x+y
+
+class Operation:
+    sum_method = sum
+
+result_sum = Operation.sum_method(200,199)
+print(result_sum)
+
+# the methods in a class can call another methods with the self refence 
+
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x):
+        self.data.append(x)
+
+    def addtwice(self, x):
+        self.add(x)
+        self.add(x)
+
+# To use inheritance send at the deninition name the class that inheritance
+#class Name(Address)
+#class Name(Address,Location,Estate) # for multiple inheritance
+
+# To define a private variavle use __ to the private variable
+
+class Location:    
+
+    def you_are(self):
+        self.__location = "Colombia"
+        text = "Your Country is: " + self.__location 
+        return text
+
+loc = Location()
+print(loc.you_are())
+
+
 
 
 
