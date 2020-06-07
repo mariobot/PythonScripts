7c85c15eb1dd378d0755b9949cea012a8c3342d5
mariobot <mariobot113@hotmail.com>
2020-06-07 18:38:52 -0500

    Add Strings, Numbers, Introduction

---
+++ 2_Interpreter/hi_python.py
@@ -0,0 +1,11 @@
+# https://docs.python.org/3/tutorial/interpreter.html#
+
+# To print in console use print()
+print("Hello Python")
+
+# basic script
+I_Whant_To_Learn = True
+if I_Whant_To_Learn:
+    print("Welcome to this magic tutorial to learn somethin about Python!")
+else:
+    print("I recomend to you Netflix")
